86fbd7340e449b3f2f4228ebe3b0116e87ea389e
RuanFigueiredo <ruanthiago67@gmail.com>
2024-02-15 11:41:35 -0300

    primeira  operação realizada +

---
+++ script.js
@@ -0,0 +1,22 @@
+function calcular(){
+    let valor1 = Number(document.getElementById("primeiroValor").value)
+    let valor2 = Number(document.getElementById("segundoValor").value)
+    const option = document.querySelector("input[name='option']:checked").value
+
+    if((valor1 >=0 && valor1 <= 100) && (valor2 >=0 && valor2 <=100)){
+        switch(option){
+            case "+":
+                document.getElementById("resultado").innerHTML = valor1 + valor2;
+                break;
+            case "-":
+                break
+            case "*":
+                break
+            case "/":
+                break;
+        }
+    }else {
+        alert("digite valores válidos");
+    }
+    
+}
